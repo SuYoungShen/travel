63084a5cacede706a7bc9131b07f4bad8477d61a
unknown <k90218104@gcloud.csu.edu.tw>
2018-02-28 23:46:40 +0800

    test

--- application/views/footer.php
+++ application/views/footer.php
@@ -16,7 +16,7 @@
     </ul>
   </nav>
   <!-- nav -->
-s
+
   <ul role="social-icons">
     <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
     <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
