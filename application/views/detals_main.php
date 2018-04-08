8d9304ef1bdf66b45e1861cf2e2826bc68dbf354
unknown <k90218104@gcloud.csu.edu.tw>
2018-04-09 01:14:18 +0800

    加入我的最愛成功 in 20180409

--- application/views/detals_main.php
+++ application/views/detals_main.php
@@ -2,6 +2,7 @@
 .swal2-popup{
   font-size: 2rem;
 }
+
 </style>
 <main role="main-inner-wrapper" class="container" onload="initialize()" onunload="GUnload()">
     <div class="blog-details">
@@ -48,19 +49,18 @@
         <div class="enter-content">
 
           <p class="bg-info"><?=$Introduction;?>
-            <button type="button" class="btn btn-success">
-              <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-            </button>
+            <button type="button" class="btn btn_like
+            <?php if($user_like_total==1){?>
+              btn-danger
+            <?php }else{ ?>
+              btn-success
+            <?php } ?>
+            ">
+            <input type="hidden" class="user_like_id" value="<?=isset($user_like->id)?$user_like->id:"";?>">
+            <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+
+          </button>
           </p>
-          <script type="text/javascript">
-            $(document).ready(function() {
-              $('.btn').click(function(event) {
-                /* Act on the event */
-                $(this).toggleClass('btn-danger');
-                
-              });
-            });
-          </script>
           <p class="bg-danger">
             <?=$OpenTime;?><br/>
             <?=$Tel;?><br/>
@@ -119,6 +119,7 @@
             <div class="row">
               <form>
                 <div class="col-xs-12 col-sm-6 col-md-6">
+                  <!-- 景點ID -->
                   <input type="hidden" name="Id" value="<?=$Id;?>">
                   <input name="Post_Name" type="text"
                   placeholder="<?=$this->session->userdata('user_name');?>"
@@ -148,11 +149,12 @@
   </div>
 </main>
 
+
 <script type="text/javascript">
-//20180325
+//20180325 AMessage
   $(document).ready(function() {
+    var Id = $("input[name='Id']").val();
     $("#Submit").click(function(event) {
-      var Id = $("input[name='Id']").val();
       var Post_Name = $("input[name='Post_Name']").val();
       var Post_Email = $("input[name='Post_Email']").val();
       var Message = $("textarea[name='Message']").val();
@@ -177,7 +179,6 @@
           error: function(data){
             if (data.sys_code === 404) {
               swalls('Error',datas.sys_msg, 'danger');
-
             }
           }
         });
@@ -189,15 +190,67 @@
         );
       }
     });
+
+    // add 愛心功能  in 20180407
+    $('.btn_like').click(function(event) {
+      var api = 'user_like';
+
+      //最愛id in 20180409
+      var user_like_id = $('.user_like_id').val();
+      
+      //紅色愛心=已加入最愛 in 20180409
+      if ($(this).hasClass('btn-danger')) {
+        var action = 'delete';//刪除最愛 in 20180409
+        user_like_ajax(api, Id, action, user_like_id);
+        $(this).attr('class', 'btn btn-like btn-success');
+      }else if($(this).hasClass('btn-success')){//綠色愛心=為加入最愛 in 20180409
+        var action = 'insert';//加入最愛 in 20180409
+        user_like_ajax(api, Id, action, user_like_id);
+        $(this).attr('class', 'btn btn-like btn-danger');
+      }
+
+    });//End btn_like
   });
 
-  function swalls(statuss, sys_msg, status){
+  //20180409 user_like ajax
+  function user_like_ajax(api, Id, action, user_like_id){
+    $.ajax({
+      url: api,
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        id: user_like_id,
+        action: action,//加入或刪除
+        place_id: Id,//景點id
+        place: '<?=$this->uri->segment(3);?>'//地區名
+      }
+    })
+    .done(function(resOK) {
+      if(resOK.sys_code == 200){
+        swal(resOK.sys_msg_title, resOK.sys_msg, resOK.status).then(function(){
+          location.reload();
+        });
+        $('.btn_like').toggleClass("btn-danger");//Red愛心 in 20180409
+
+        $('.btn_like').toggleClass('btn-success');//Green 愛心 in 20180409
+      }else if(resOK.sys_code == 404){
+        swal(resOK.sys_msg_title, resOK.sys_msg, resOK.status).then(function (){
+          window.open('login');//開新頁 in 20180407
+        });
+      }
+    })
+    .fail(function(resError) {
+      console.log("error");
+    });
+  }
+
+  function swalls(sys_msg_title, sys_msg, status){
     swal(
-      statuss,
+      sys_msg_title,
       sys_msg,
       status
     ).then(function (){
-      location.reload();
+      location.reload('');
     });
   }
 </script>
