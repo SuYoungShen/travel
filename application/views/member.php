7283af7f044429d4916a5c1ca8a5f5db856505da
unknown <k90218104@gcloud.csu.edu.tw>
2018-04-09 03:42:38 +0800

    add user_like 的功能 in 20180409

--- application/views/member.php
+++ application/views/member.php
@@ -91,7 +91,7 @@
       <ul class="comments-reply">
           <h3>景點留言</h3>
         <li>
-          <table id="example" class="table table-striped table-hover table-bordered dt-responsive nowrap" style="width:100%">
+          <table id="AM" class="table table-striped table-hover table-bordered dt-responsive nowrap" style="width:100%">
           <thead>
             <tr>
               <th>地區</th>
@@ -133,16 +133,36 @@
           </section> -->
         </li>
 
+        <h3>我的最愛</h3>
         <li>
-          <figure>
-            <!-- <img src="images/blog-images/image-3.jpg" alt="" class="img-responsive"/> -->
-          </figure>
+          <table id="user_like" class="table table-striped table-hover table-bordered dt-responsive nowrap" style="width:100%">
+            <thead>
+              <tr>
+                <th>地區</th>
+                <th>景點名</th>
+                <th>開放時間</th>
+                <th>電話</th>
+                <th>地址</th>
+                <th>加入最愛時間</th>
+              </tr>
+            </thead>
+            <tbody>
 
-          <section>
-            <h4>Anna Greenfield  <a href="#">Reply</a></h4>
-            <div class="date-pan">January 26, 2016</div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat eu nibh ultricies semper. Vivamus porta, felis vitae facilisis sodales, felis est iaculis orci, et ornare sem mauris ut turpis. Pellentesque vitae tortor nec tellus hendrerit aliquam. Donec condimentum leo eu ullamcorper scelerisque pellentesque urna rhoncus.
-          </section>
+              <?php for ($i=0; $i < count($user_like); $i++) { ?>
+                <tr class="info" data-id=<?=$user_like[$i]['id'];?>>
+                  <?php for ($j=0; $j < 1; $j++){ ?>
+                    <td><?=$Att_like[$i][$j]['ch_place'];?></td>
+                    <td><?=$Att_like[$i][$j]['Name'];?></td>
+                    <td><?=$Att_like[$i][$j]['Opentime'];?></td>
+                    <td><?=$Att_like[$i][$j]['Tel'];?></td>
+                    <td><?=$Att_like[$i][$j]['Add'];?></td>
+                  <?php } ?>
+                  <td><?=$user_like[$i]['create_date'].'|'.$user_like[$i]['create_time'];?></td>
+                </tr>
+              <?php } ?>
+
+            </tbody>
+          </table>
         </li>
       </ul>
 
@@ -181,63 +201,110 @@
 
 <script type="text/javascript">
 $(document).ready(function() {
-  $('#example').DataTable();
-  $('#example2').DataTable();
+  $('#AM').DataTable();
+  $('#user_like').DataTable();
   $('tr').on('click', function (event) {
 
     var id = $(this).data("id");
+    if ($(this).hasClass('success')) {//景點留言 in 20180409
 
-    $.ajax({
-      url: 'Att_and_Am',
-      type: 'POST',
-      dataType: 'json',
-      data: {id: id}
-    })
-    .done(function(res) {
+      $.ajax({
+        url: 'Att_and_Am',
+        type: 'POST',
+        dataType: 'json',
+        data: {id: id}
+      })
+      .done(function(res) {
 
-      swal({
-        title: res.Place.ch_place+"-"+res.Att.Name,
-        imageUrl: res.Att.Picture,
-        html:"<p class='bg-info' style='font-size:20px;'>"+res.Att.Description+"</p><p class='bg-danger text-left'>開放時間："+res.Att.Opentime+"<br/>電話："+res.Att.Tel+"<br/>地址："+res.Att.Add+"</p><p class='bg-success text-left'>您的留言："+res.Am.message+"</p>",
-        imageAlt: 'Custom image',
-        showCancelButton: true,
-        cancelButtonText:　"關閉",
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: '刪除留言',
-        confirmButtonColor: '#d33'
-      }).then((result) => {
-        if (result.value) {
-          // $.post('Delete_Am',{am_id: res.Am.am_id,id: res.Am.id,email: res.Am.email}, function(res){
-          //   console.log(res);
-          // }, 'json');
-
-          $.ajax({
-            url: 'Delete_Am',
-            type: 'POST',
-            dataType: 'json',
-            data: {
-              am_id: res.Am.am_id,
-              id: res.Am.id,
-              email: res.Am.email
-            }
-          })
-          .done(function(resOK) {
-            if(resOK.sys_code == 200 || resOK.sys_code == 404){
-              swal(
-                resOK.sys_title,
-                resOK.sys_msg,
-                resOK.status
-              ).then(function(){
-                location.reload();
-              });
-            }//End if
-          });//End done
-        }
+        swal({
+          title: res.Place.ch_place+"-"+res.Att.Name,
+          imageUrl: res.Att.Picture,
+          html:"<p class='bg-info' style='font-size:20px;'>"+res.Att.Description+"</p><p class='bg-danger text-left'>開放時間："+res.Att.Opentime+"<br/>電話："+res.Att.Tel+"<br/>地址："+res.Att.Add+"</p><p class='bg-success text-left'>您的留言："+res.Am.message+"</p>",
+          imageAlt: 'Custom image',
+          showCancelButton: true,
+          cancelButtonText:　"關閉",
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: '刪除留言',
+          confirmButtonColor: '#d33'
+        }).then((result) => {
+          if (result.value) {
+            $.ajax({
+              url: 'delete_Am',
+              type: 'POST',
+              dataType: 'json',
+              data: {
+                am_id: res.Am.am_id,
+                id: res.Am.id,
+                email: res.Am.email
+              }
+            })
+            .done(function(resOK) {
+              if(resOK.sys_code == 200 || resOK.sys_code == 404){
+                swal(
+                  resOK.sys_title,
+                  resOK.sys_msg,
+                  resOK.status
+                ).then(function(){
+                  location.reload();
+                });
+              }//End if
+            });//End done
+          }
+        });
+      })
+      .fail(function() {
+        console.log("error");
       });
-    })
-    .fail(function() {
-      console.log("error");
-    });
+
+    }else if($(this).hasClass('info')){//我的最愛 in 20180409
+
+      $.ajax({
+        url: 'Att_and_UL',//Ul = user_like
+        type: 'POST',
+        dataType: 'json',
+        data: {id: id}
+      })
+      .done(function(res) {
+        console.log(res);
+        swal({
+          title: res.Place.ch_place+"-"+res.Att.Name,
+          imageUrl: res.Att.Picture,
+          html:"<p class='bg-info' style='font-size:20px;'>"+res.Att.Description+"</p><p class='bg-danger text-left'>開放時間："+res.Att.Opentime+"<br/>電話："+res.Att.Tel+"<br/>地址："+res.Att.Add+"</p>",
+          imageAlt: 'Custom image',
+          showCancelButton: true,
+          cancelButtonText:　"關閉",
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: '移除我的最愛',
+          confirmButtonColor: '#d33'
+        }).then((result) => {
+          if (result.value) {
+            $.ajax({
+              url: 'delete_UL',
+              type: 'POST',
+              dataType: 'json',
+              data: {
+                like_id: res.user_like.id,
+                place_id: res.user_like.place_id
+              }
+            })
+            .done(function(resOK) {
+              if(resOK.sys_code == 200 || resOK.sys_code == 404){
+                swal(
+                  resOK.sys_title,
+                  resOK.sys_msg,
+                  resOK.status
+                ).then(function(){
+                  location.reload();
+                });
+              }//End if
+            });//End done
+          }
+        });
+      })
+      .fail(function() {
+        console.log("error");
+      });
+    }
 
   });
 });
