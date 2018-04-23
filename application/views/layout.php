94180f213f77fe79af5e91e0061d2c8861bda32d
unknown <k90218104@gcloud.csu.edu.tw>
2018-04-24 01:13:14 +0800

    忘記更新什麼 in 20180424

--- application/views/layout.php
+++ application/views/layout.php
@@ -35,8 +35,8 @@
      <!-- jquery cookie -->
 
      <!-- DataTable -->
-     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.bootstrap.min.css">
-     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+     <link rel="stylesheet" href="assets/css/responsive.bootstrap.min.css">
+     <link rel="stylesheet" href="assets/css/dataTables.bootstrap.min.css">
      <!-- DataTable -->
      <?php
      if ($this->uri->segment(1) == 'details'){
