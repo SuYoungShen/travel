56d403cb1870014f9c783204c625b23f7cb1af32
unknown <k90218104@gcloud.csu.edu.tw>
2018-05-06 23:56:06 +0800

    delete_place -> do_place 用於編輯、刪除地區用，增加編輯地區功能 in 20180506

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -8,6 +8,7 @@ class Api extends CI_Controller {
 		parent::__construct();
 		$this->load->model('travel_model');
 		$this->load->model('travel_member');
+		$this->load->model('bs_model');
 	}
 
   function third_Fb_Login(){
@@ -211,28 +212,50 @@ class Api extends CI_Controller {
 	}
 
 	//20180430刪除地區功能
-	function delete_place(){
+	function do_place(){
 		$dataArray = array(
 			'rule' => $this->input->post('rule'),
 			'place_id' => $this->input->post('place_id')
 		);
+		if (!empty($dataArray['place_id'])) {
+			if (!empty($dataArray['rule']) && $dataArray['rule'] == "Delete") {
 
-		if (!empty($dataArray['rule']) && $dataArray['rule'] == "Delete" && !empty($dataArray['place_id'])) {
+				$where = "id="."'".$dataArray['place_id']."'";
+				if ($this->bs_model->delete('place', $where)) {
+					$view_data['sys_code'] = 200;
+					$view_data['sys_title'] = '成功';
+					$view_data['sys_msg'] = "恭喜刪除成功!!!";
+					$view_data['status'] = 'warning';
 
-			$where = "id="."'".$dataArray['place_id']."'";
-			if ($this->travel_model->delete('place', $where)) {
-				$view_data['sys_code'] = 200;
-				$view_data['sys_title'] = '成功';
-				$view_data['sys_msg'] = "恭喜刪除成功!!!";
-				$view_data['status'] = 'success';
+				}else {
+					$view_data['sys_code'] = 404;
+					$view_data['sys_title'] = '失敗';
+					$view_data['sys_msg'] = "刪除地區失敗!!!";
+					$view_data['status'] = 'error';
+				}
+				$this->output->set_content_type('application/json')->set_output(json_encode($view_data));
+			}else if(!empty($dataArray['rule']) && $dataArray['rule'] == "Edit"){
+				//增加編輯功能 in 201080506
+				$data = array(
+					'ch_place' => $this->input->post('ed_ch_place'),
+					'en_place' => $this->input->post('ed_en_place')
+				);
 
-			}else {
-				$view_data['sys_code'] = 404;
-				$view_data['sys_title'] = '失敗';
-				$view_data['sys_msg'] = "刪除留言失敗!!!";
-				$view_data['status'] = 'error';
+				$where = "id="."'".$dataArray['place_id']."'";
+				if ($this->bs_model->update('place', $data, $where)) {
+					$view_data['sys_code'] = 200;
+					$view_data['sys_title'] = '成功';
+					$view_data['sys_msg'] = "恭喜更新成功!!!";
+					$view_data['status'] = 'success';
+
+				}else {
+					$view_data['sys_code'] = 404;
+					$view_data['sys_title'] = '失敗';
+					$view_data['sys_msg'] = "更新地區失敗!!!";
+					$view_data['status'] = 'error';
+				}
+				$this->output->set_content_type('application/json')->set_output(json_encode($view_data));
 			}
-			$this->output->set_content_type('application/json')->set_output(json_encode($view_data));
 		}
 	}
 
