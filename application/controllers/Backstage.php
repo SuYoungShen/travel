8f23d6d3a6f18d86f6cf1bb8e50e80c58dfbe212
unknown <k90218104@gcloud.csu.edu.tw>
2018-05-07 11:22:02 +0800

    add 刪除、編輯 in 20180507

--- application/controllers/Backstage.php
+++ application/controllers/Backstage.php
@@ -63,6 +63,55 @@ class Backstage extends CI_Controller {
     $this->load->view('backstage/layout', $view_data);
   }
 
+	//add 刪除、編輯地區功能 in 20180507
+	function do_place(){
+		$dataArray = array(
+			'rule' => $this->input->post('rule'),
+			'place_id' => $this->input->post('place_id')
+		);
+		if (!empty($dataArray['place_id'])) {
+			if (!empty($dataArray['rule']) && $dataArray['rule'] == "Delete") {
+
+				$where = "id="."'".$dataArray['place_id']."'";
+				if ($this->bs_model->delete('place', $where)) {
+					$view_data['sys_code'] = 200;
+					$view_data['sys_title'] = '成功';
+					$view_data['sys_msg'] = "恭喜刪除成功!!!";
+					$view_data['status'] = 'warning';
+
+				}else {
+					$view_data['sys_code'] = 404;
+					$view_data['sys_title'] = '失敗';
+					$view_data['sys_msg'] = "刪除地區失敗!!!";
+					$view_data['status'] = 'error';
+				}
+				$this->output->set_content_type('application/json')->set_output(json_encode($view_data));
+			}else if(!empty($dataArray['rule']) && $dataArray['rule'] == "Edit"){
+				//增加編輯功能 in 201080506
+				$data = array(
+					'ch_place' => $this->input->post('ed_ch_place'),
+					'en_place' => $this->input->post('ed_en_place')
+				);
+
+				$where = "id="."'".$dataArray['place_id']."'";
+				if ($this->bs_model->update('place', $data, $where)) {
+					$view_data['sys_code'] = 200;
+					$view_data['sys_title'] = '成功';
+					$view_data['sys_msg'] = "恭喜更新成功!!!";
+					$view_data['status'] = 'success';
+
+				}else {
+					$view_data['sys_code'] = 404;
+					$view_data['sys_title'] = '失敗';
+					$view_data['sys_msg'] = "更新地區失敗!!!";
+					$view_data['status'] = 'error';
+				}
+				$this->output->set_content_type('application/json')->set_output(json_encode($view_data));
+			}
+		}
+	}//do_place
+
+
 	function attractions()
   {
 		$view_data = array(
