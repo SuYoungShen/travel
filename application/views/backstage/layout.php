8f5c9321a598abcb749b8dc7379f726e08b2feba
sushen <k90218104@gcloud.csu.edu.tw>
2018-08-21 10:22:02 +0800

    沒事 in 20180821

--- application/views/backstage/layout.php
+++ application/views/backstage/layout.php
@@ -13,22 +13,22 @@
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="../assets/backstage/css/normalize.css">
-    <link rel="stylesheet" href="../assets/backstage/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/backstage/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/backstage/css/themify-icons.css">
-    <link rel="stylesheet" href="../assets/backstage/css/flag-icon.min.css">
-    <link rel="stylesheet" href="../assets/backstage/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="../assets/backstage/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="assets/backstage/css/normalize.css">
+    <link rel="stylesheet" href="assets/backstage/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/backstage/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/backstage/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/backstage/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/backstage/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="assets/backstage/css/dataTables.bootstrap.min.css">
     <!-- <link rel="stylesheet" href="../assets/css/bootstrap-select.less"> -->
-    <link rel="stylesheet" href="../assets/backstage/css/style.css">
-    <link rel="stylesheet" href="../assets/css/sweetalert2.min.css">
+    <link rel="stylesheet" href="assets/backstage/css/style.css">
+    <link rel="stylesheet" href="assets/css/sweetalert2.min.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-    <script src="../assets/backstage/js/jquery-2.1.4.min.js"></script>
+    <script src="assets/backstage/js/jquery-2.1.4.min.js"></script>
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
-  
+
 </head>
 <body>
   <?php include 'menu.php'; ?>
